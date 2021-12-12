d5972a9a842eaaf1b29af4c929ae74ce94658613
korneichikpavelvictorovich@gmail.com <korneichikpavelvictorovich@gmail.com>
2021-12-12 20:52:36 +0300

    Merge branch 'Tests2Branch' of https://github.com/PavelKorney/Test1 into Tests2Branch+

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
             System.out.println("!!!!!!!!!!!!!!!!!!");
             System.out.println("World");
             System.out.println("Pavel");
+            System.out.println("Inga");
 
         }
     }
