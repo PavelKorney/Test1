57a113ee943cd25f98511d5df5fb186afeb715d7
korneichikpavelvictorovich@gmail.com <korneichikpavelvictorovich@gmail.com>
2021-12-12 20:09:27 +0300

    init + miniProg + !!!! + world+Pasha

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ public class Main {
             }
             System.out.println("!!!!!!!!!!!!!!!!!!");
             System.out.println("World");
+            System.out.println("Pavel");
 
         }
     }
